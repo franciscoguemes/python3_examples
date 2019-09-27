d8ab896a149390e1711a355560637bc4f4bf413f
Francisco Güemes <francisco.gueemes@uniscon.net>
2019-09-27 10:00:33 +0200

    Updated comment.

--- tkinter/02_helloWorld.py
+++ tkinter/02_helloWorld.py
@@ -7,7 +7,7 @@ from tkinter import Label
 top = tkinter.Tk()
 
 top.title('Welcome to my examples')
-top.geometry('500x100') #Set the size of the window to 500x500
+top.geometry('500x100') #Set the size of the window to 500x100
 top.resizable(0, 0) #Don't allow resizing in the x or y direction
 
 # Add a label with the text...
